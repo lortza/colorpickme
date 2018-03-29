59a20dd0d36da688ba30f26dade4600d565fbfea
anne <richardson.ae@gmail.com>
2018-03-29 12:19:51 -0500

    Refines styles and dropdown

--- scripts.js
+++ scripts.js
@@ -1,28 +1,50 @@
+const opacities = [
+  {display: '100%', val: 1},
+  {display: '90%', val: 0.9},
+  {display: '80%', val: 0.8},
+  {display: '70%', val: 0.7},
+  {display: '60%', val: 0.6},
+  {display: '50%', val: 0.5},
+  {display: '40%', val: 0.4},
+  {display: '30%', val: 0.3},
+  {display: '20%', val: 0.2},
+  {display: '10%', val: 0.1}
+]
 
-const mouseCoords = document.querySelector('#mouse-coords')
+const optionsPicker = document.querySelector('#options-picker')
+const mouseZone = document.querySelector('#mouse-zone')
 const rgbOutput = document.querySelector('#rgb-output')
-const mouseZone = document.querySelector('article')
-const rgbField = document.querySelector('#rgb-field')
+let opacity = 1;
 
 const reportColors = function(event){
-  let rgb = `rgb(${event.offsetX}, ${event.offsetY}, ${Math.round( (event.offsetX / 2) + (event.offsetY /2) ) })`
-  mouseCoords.textContent = `MouseX: ${event.offsetX} MouseY: ${event.offsetY}`
-  rgbOutput.textContent = rgb
-  rgbField.setAttribute('value', rgb)
-  document.body.style.backgroundColor = rgb
-}
+  let red = event.offsetX
+  let green = event.offsetY
+  let blue = Math.round( (event.offsetX / 5) + (event.offsetY / 3) )
+  let rgb = `rgb(${red}, ${green}, ${blue}, ${opacity})`
 
-const setToDefault = function(){
-  mouseCoords.textContent = "Mouse Coords:"
-  rgbOutput.textContent = "RGB Output:"
+  rgbOutput.setAttribute('value', rgb)
+  rgbOutput.style.color = rgb
+  mouseZone.style.backgroundColor = rgb
 }
 
 const copyToClipboard = function(event){
-  rgbField.select();
+  rgbOutput.select();
   document.execCommand("Copy");
-  alert(rgbField.value + " is copied to your clipboard");
+  // alert(rgbOutput.value + " is copied to your clipboard");
 }
 
+// Handle the mouse movement and click behaviors
 mouseZone.addEventListener('mousemove', reportColors)
-mouseZone.addEventListener('mouseleave', setToDefault)
 mouseZone.addEventListener('click', copyToClipboard)
+
+
+// Handle the dropdown actions
+let htmlOptions = opacities.map(opacity => `<option value="${opacity.display}">${opacity.display}</option>`).join('')
+optionsPicker.insertAdjacentHTML('beforeend', htmlOptions)
+
+optionsPicker.addEventListener('change', function(){
+  let optText = this.options[this.selectedIndex].value
+  let opacityObj = opacities.find(function(op){ return op.display === optText })
+  opacity = opacityObj.val
+})
+
