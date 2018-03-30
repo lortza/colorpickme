890a82953952156ab5d93325322d46336c011741
anne <richardson.ae@gmail.com>
2018-03-30 11:27:21 -0500

    Refactors color picker behaviors into class

--- scripts.js
+++ scripts.js
@@ -1,3 +1,76 @@
+class ColorPicker {
+  constructor(){
+    this.red = 200
+    this.green = 121
+    this.blue = 90
+    this.opacity = 1
+  }
+
+  updateColors(red, green, blue) {
+    this.red = red
+    this.green = green
+    this.blue = blue
+  }
+
+  updateOpacity(opacity) {
+    this.opacity = opacity
+  }
+
+  rgb() {
+    return `rgba(${this.red},${this.green},${this.blue},${this.opacity})`
+  }
+
+  hex() {
+    return `#${this.componentToHex(this.red)}${this.componentToHex(this.green)}${this.componentToHex(this.blue)}`
+  }
+
+  componentToHex(n) {
+    n = parseInt(n,10);
+    if (isNaN(n)) return "00";
+    n = Math.max(0,Math.min(n,255));
+    return "0123456789ABCDEF".charAt((n-n%16)/16)
+    + "0123456789ABCDEF".charAt(n%16);
+  }
+
+}//end ColorPicker
+
+let colorPicker = new ColorPicker()
+
+const opacityDropdown = document.querySelector('#opacity-dropdown')
+const mouseZone = document.querySelector('#mouse-zone')
+const rgbOutput = document.querySelector('#rgb-output')
+const hexOutput = document.querySelector('#hex-output')
+const rgbButton = document.querySelector('#rgb-button')
+const hexButton = document.querySelector('#hex-button')
+
+
+function renderColorsToDom(){
+  rgbOutput.setAttribute('value', colorPicker.rgb())
+  rgbOutput.style.color = colorPicker.rgb()
+
+  hexOutput.setAttribute('value', colorPicker.hex())
+  hexOutput.style.color = colorPicker.hex()
+
+  mouseZone.style.backgroundColor = colorPicker.rgb()
+}
+
+function assignColors(event){
+  colorPicker.updateColors(event.offsetX, event.offsetY, Math.round( (event.offsetX / 5) + (event.offsetY / 3) ))
+  renderColorsToDom()
+}
+
+function copyToClipboard(source) {
+  // use anon function to access the callback's param
+  return function() {
+    source.select();
+    document.execCommand("Copy");
+  }
+}
+
+
+//-----------------------------------------
+
+// Populate the dropdown items
 const opacities = [
   {display: '100%', val: 1},
   {display: '90%', val: 0.9},
@@ -11,78 +84,24 @@ const opacities = [
   {display: '10%', val: 0.1}
 ]
 
-const optionsPicker = document.querySelector('#options-picker')
-const mouseZone = document.querySelector('#mouse-zone')
-const rgbOutput = document.querySelector('#rgb-output')
-const hexOutput = document.querySelector('#hex-output')
-const rgbButton = document.querySelector('#rgb-button')
-const hexButton = document.querySelector('#hex-button')
-
-let red = 250
-let green = 121
-let blue = 90
-let opacity = 1
-let rgb = `rgba(${red},${green},${blue},${opacity})`
-let hex = rgbToHex(red, green, blue)
-
-// Convert to hex
-function componentToHex(n) {
-  n = parseInt(n,10);
-  if (isNaN(n)) return "00";
-  n = Math.max(0,Math.min(n,255));
-  return "0123456789ABCDEF".charAt((n-n%16)/16)
-  + "0123456789ABCDEF".charAt(n%16);
-}
-
-function rgbToHex(R,G,B) {
-  return `#${componentToHex(R)}${componentToHex(G)}${componentToHex(B)}`
-}
-
-function assignColors(event){
-  red = event.offsetX
-  green = event.offsetY
-  blue = Math.round( (event.offsetX / 5) + (event.offsetY / 3) )
-  reportColors()
-}
-
-function reportColors(){
-  rgb = `rgba(${red},${green},${blue},${opacity})`
-  hex = rgbToHex(red, green, blue)
-  rgbOutput.setAttribute('value', rgb)
-  rgbOutput.style.color = rgb
-  hexOutput.setAttribute('value', hex)
-  hexOutput.style.color = hex
-  console.log(hex);
-  mouseZone.style.backgroundColor = rgb
-}
-
-function copyRgbToClipboard(event){
-  rgbOutput.select();
-  document.execCommand("Copy");
-}
-
-function copyHexToClipboard(event){
-  hexOutput.select();
-  document.execCommand("Copy");
-}
-
-// Handle the mouse movement and click behaviors
-reportColors()
-mouseZone.addEventListener('mousemove', assignColors)
-mouseZone.addEventListener('click', copyRgbToClipboard)
-rgbButton.addEventListener('click', copyRgbToClipboard)
-hexButton.addEventListener('click', copyHexToClipboard)
-
-
-// Handle the dropdown actions
 let htmlOptions = opacities.map(opacity => `<option value="${opacity.display}">${opacity.display}</option>`).join('')
-optionsPicker.insertAdjacentHTML('beforeend', htmlOptions)
+opacityDropdown.insertAdjacentHTML('beforeend', htmlOptions)
+
+
+//-----------------------------------------
+
 
 // Update the opacity of the current color
-optionsPicker.addEventListener('change', function(){
+opacityDropdown.addEventListener('change', function(){
   let optText = this.options[this.selectedIndex].value
   let opacityObj = opacities.find(function(op){ return op.display === optText })
-  opacity = opacityObj.val
-  reportColors()
+  colorPicker.updateOpacity(opacityObj.val)
+  renderColorsToDom()
 })
 
+// Handle the mouse movement and click behaviors
+renderColorsToDom()
+mouseZone.addEventListener('mousemove', assignColors)
+mouseZone.addEventListener('click', copyToClipboard(rgbOutput))
+rgbButton.addEventListener('click', copyToClipboard(rgbOutput))
+hexButton.addEventListener('click', copyToClipboard(hexOutput))
